67131cd6a1d2ae17f065c5f26797ae5da1bc426c
mikan12160927 <mikan12160927@gmail.com>
2026-01-11 16:15:31 +0900

    Update view_script.js

--- view_script.js
+++ view_script.js
@@ -51,11 +51,7 @@ async function fetchAndDisplayItems(clickedButtonId) {
 
         const date = new Date(item.date_time);
         const formattedDate = date.toLocaleString('ja-JP', {
-            year: 'numeric',
-            month: '2-digit',
-            day: '2-digit',
-            hour: '2-digit',
-            minute: '2-digit'
+            year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit'
         });
 
         card.innerHTML = `
